2550d4e4049e528c6e58aa590388faa089de8252
pkoleva <koleva.pavlina@abv.bg>
2016-05-18 11:39:43 +0300

    Added a couple of tests for VSCode Extension

--- PocDebugger/Lib/Common.sikuli/Common.py
+++ PocDebugger/Lib/Common.sikuli/Common.py
@@ -18,7 +18,7 @@ def start_console():
 
 
 def execute_command(command):
-    sshproc = subprocess.Popen(["start cmd /c " + command],
+    sshproc = subprocess.Popen([command],
                                shell=True,
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
@@ -40,6 +40,37 @@ def capture_screen(name):
     os.rename(file_name, name)
 
 
-def typeInField(field,string):
+def type_infield(field, string):
     click(field)
     type(string)
+
+
+def wait_to_load(image, counter):
+    n = counter
+    a = False
+    while n != 0:
+        if exists(image):
+            print("Found image" + repr(image))
+            a = True
+            break
+        else:
+            wait(1)
+        n -= 1
+    print("Waited for " + repr(counter-n) + " seconds")
+    return a
+
+# def open_tab(tab):
+
+
+def generate_debug_configurations():
+    click(ImageLibrary.Common.settingsNewBtn)
+    wait_to_load(ImageLibrary.Common.selectEnvironmentLbl,2)
+    click(ImageLibrary.Common.nativeScriptLbl)
+
+
+def run_configuration(configuration):
+    click(Pattern(ImageLibrary.Common.settingsBtn).targetOffset(-50, 0))
+    wait(configuration)
+    click(Pattern(configuration).similar(0.9))
+    click(ImageLibrary.Common.startDebugButton)
+
--- PocDebugger/Tests/CommandLine_Tests.sikuli/CommandLine_Tests.py
+++ PocDebugger/Tests/CommandLine_Tests.sikuli/CommandLine_Tests.py
@@ -4,7 +4,6 @@ import os
 import sys
 import unittest
 from Lib import Common
-from Lib import ImageLibrary
 from Common import *
 from ImageLibrary import *
 
@@ -12,7 +11,7 @@ from ImageLibrary import *
 cwd = os.getcwd()
 
 
-class CommandLineTests(unittest.TestCase):
+class CommandLine_Tests(unittest.TestCase):
     global cwd
 
     def setUp(self):
@@ -25,8 +24,81 @@ class CommandLineTests(unittest.TestCase):
             capture_screen(name)
             os.remove(name)
 
-    def test001(self):
-        start_console()
-        typeInField(Pattern(Common.consoleWindow).similar(0.9), "tns create TestApp")
-        type(Key.ENTER)
-        wait(Common.projectCreated, 10)
+    # def test001(self):
+    #     start_console()
+    #     typeInField(Pattern(Common.consoleWindow).similar(0.9), "tns create TestApp")
+    #     type(Key.ENTER)
+    #     wait(Common.projectCreated, 10)
+    #     typeInField(Pattern(Common.consoleWindow).similar(0.9), "cd TestApp")
+
+    # def test100_LaunchAndroidEmulator_VerifyStarted(self):
+    #     wait_to_load(Common.debugTab, 2)
+    #     if not exists(Common.debugLbl):
+    #         click(Common.debugTab)
+    #         wait_to_load(Common.debugLbl, 2)
+    #     if exists(Pattern(Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(Common.launchAndroidEmuLbl)
+
+    # def test110_AttachAndroidEmulator_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.attachAndroidEmulatorLbl)
+
+    # def test200_LaunchAndroidDevice_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.launchAndroidDeviceLbl)
+
+    # def test210_AttachAndroidDevice_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.attachAndroidDeviceLbl)
+
+    # def test300_LaunchiOSEmulator_VerifyStarted(self):
+    #     wait_to_load(Common.debugTab, 2)
+    #     if not exists(Common.debugLbl):
+    #         click(Common.debugTab)
+    #         wait_to_load(Common.debugLbl, 2)
+    #     if exists(Pattern(Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(Common.launchiOSEmulatorLbl)
+
+    # def test310_AttachiOSEmulator_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.attachiOSEmulatorLbl)
+
+    # def test400_LaunchiOSDevice_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.launchiOSDeviceLbl)
+
+    # def test410_AttachiOSDevice_VerifyStarted(self):
+    #     wait_to_load(ImageLibrary.Common.debugTab, 2)
+    #     if not exists(ImageLibrary.Common.debugLbl):
+    #         click(ImageLibrary.Common.debugTab)
+    #         wait_to_load(ImageLibrary.Common.debugLbl, 2)
+    #     if exists(Pattern(ImageLibrary.Common.settingsNewBtn).similar(0.9)):
+    #         generate_debug_configurations()
+    #     run_configuration(ImageLibrary.Common.attachiOSDeviceLbl)
